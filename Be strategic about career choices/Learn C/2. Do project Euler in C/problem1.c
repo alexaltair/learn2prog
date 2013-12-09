75a829836b14172b603b38790396d864c46e957f
Alex Altair <alexanderaltair@gmail.com>
2013-12-08 22:07:11 -0800

    Add the second project Euler problem.

--- Be strategic about career choices/Learn C/2. Do project Euler in C/problem1.c
+++ Be strategic about career choices/Learn C/2. Do project Euler in C/problem1.c
@@ -9,17 +9,17 @@ int main()
     int i;
     for (i = 0; i < ARRAY_SIZE; ++i)
     {
-      if ((i % 3 == 0) || (i % 5 == 0))
-      {
-        array[i] = i;
-      }
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            array[i] = i;
+        }
     }
 
     i = 0;
     int sum = 0;
     for (i = 0; i < ARRAY_SIZE; ++i)
     {
-      sum = sum + array[i];
+        sum = sum + array[i];
     }
 
     printf("%d\n", sum);
---
+++ Be strategic about career choices/Learn C/2. Do project Euler in C/problem2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAX 4000000
+
+int main()
+{
+    int first = 1;
+    int second = 2;
+    int third = 0;
+    int sum = 0;
+
+    int i;
+    for (i = 0; second < MAX; ++i)
+    {
+        if (second % 2 == 0)
+        {
+            sum = sum + second;
+        }
+
+        third = first + second;
+        first = second;
+        second = third;
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
