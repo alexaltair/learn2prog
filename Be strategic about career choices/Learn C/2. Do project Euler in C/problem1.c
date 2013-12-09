eaebc092dd2689927ca8fa64504c547b3d987f7f
Alex Altair <alexanderaltair@gmail.com>
2013-12-08 22:38:01 -0800

    Refactor problem 1.

--- Be strategic about career choices/Learn C/2. Do project Euler in C/problem1.c
+++ Be strategic about career choices/Learn C/2. Do project Euler in C/problem1.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-#define ARRAY_SIZE 1000
-
 int main()
 {
-    int array[ARRAY_SIZE] = {0};
+    int sum = 0;
 
     int i;
-    for (i = 0; i < ARRAY_SIZE; ++i)
+    for (i = 0; i < 1000; ++i)
     {
         if ((i % 3 == 0) || (i % 5 == 0))
         {
-            array[i] = i;
+            sum = sum + i;
         }
-    }
-
-    i = 0;
-    int sum = 0;
-    for (i = 0; i < ARRAY_SIZE; ++i)
-    {
-        sum = sum + array[i];
     }
 
     printf("%d\n", sum);
