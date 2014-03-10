b8c81d2d585c889434bb2b5a231b6989389262de
Alex Altair <alexanderaltair@gmail.com>
2014-03-09 17:40:31 -0700

    Implement improvements suggested in facebook post.

--- Be strategic about career choices/Learn C/2. Do project Euler in C/problem4.c
+++ Be strategic about career choices/Learn C/2. Do project Euler in C/problem4.c
@@ -7,22 +7,18 @@
 char * long_to_string(long number){
     int length = log10(number) + 1 + 1; // One to round up the log, and one for the '/0'.
     char * string = malloc(length);
-    return snprintf(string, length, "%ld", number);
-}
-
-void strrev(char * reversed, char * source){
-    strcpy(reversed, source);
-    int length = strlen(source);
-
-    for (int i=0; i < length; ++i){
-        reversed[i] = source[length - 1 - i];
-    }
+    snprintf(string, length, "%ld", number);
+    return string;
 }
 
 bool is_palindrome(char * string){
-    char reversed[sizeof(string) - 1];
-    strrev(reversed, string);
-    return !strcmp(string, reversed);
+    int length = strlen(string);
+    for (int i=0; i < length/2; ++i){
+        if (string[i] != string[length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(){
@@ -30,12 +26,14 @@ int main(){
     long max_palindrome = 0;
 
     for (int i = 100; i < 1000; ++i){
-        for (int j = 100; j < 1000; ++j){
+        for (int j = i; j < 1000; ++j){
             prod = i*j;
-            char * string = long_to_string(prod);
-
-            if (is_palindrome(string)){
-                max_palindrome = fmax(prod, max_palindrome);
+            if (prod > max_palindrome){
+                char * string = long_to_string(prod);
+                if (is_palindrome(string)){
+                    max_palindrome = prod;
+                }
+                free(string);
             }
         }
     }
