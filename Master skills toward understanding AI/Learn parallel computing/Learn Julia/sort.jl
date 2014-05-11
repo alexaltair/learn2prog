b57715e0276435fcdc86a1335cd71cf5f802a422
Alex Altair <alexanderaltair@gmail.com>
2014-05-11 15:39:51 -0700

    Successfully use multiple dispatch & type parameters.

--- Master skills toward understanding AI/Learn parallel computing/Learn Julia/sort.jl
+++ Master skills toward understanding AI/Learn parallel computing/Learn Julia/sort.jl
@@ -1,25 +1,30 @@
-function quicksort(array)
-  if length(array) < 2
-    return array
-  end
-
-  pivot = choose_pivot(array)
-  beginning, middle, ending = Int64[], Int64[], Int64[]
-  for element in array
-    if element < pivot
-      push!(beginning, element)
-    elseif element == pivot
-      push!(middle, element)
-    else
-      push!(ending, element)
+# Declare what kind of Array
+function quicksort{T}(array::Array{T})
+    if length(array) < 2
+        return array
     end
-  end
-  beginning = quicksort(beginning)
-  ending = quicksort(ending)
-  beginning = append!(beginning, middle)
-  append!(beginning, ending)
+
+    pivot = choose_pivot(array)
+    beginning, middle, ending = T[], T[], T[]
+    for element in array
+        if element < pivot
+            push!(beginning, element)
+        elseif element == pivot
+            push!(middle, element)
+        else
+            push!(ending, element)
+        end
+    end
+    beginning = quicksort(beginning)
+    ending = quicksort(ending)
+    beginning = append!(beginning, middle)
+    append!(beginning, ending)
 end
 
-function choose_pivot(array)
-  (array[1] + array[end])/2.0
+function choose_pivot(array::Array)
+    array[1]
+end
+
+function choose_pivot{T<:Number}(array::Array{T})
+    (array[1] + array[end])/2.0
 end
